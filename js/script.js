c4be7fa6da1ac93e9fab1dcd96f71f44909b7774
Helena Hagsma <helenahagsma2024@gmail.com>
2025-12-05 13:11:55 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,107 @@
+
+/*----------CADASTRAR SALÁRIO--------- */
+/*Seleciona o botão "Cadastrar Salário" e adiciona um evento de clique */
+document.getElementById("cadastrarSalario").addEventListener("click", function () {
+    /*Pega o valor digitado no input de salário*/
+    var salario = document.getElementById("salario").value;
+    /*Verifica se o campo está vazio*/
+    if (salario === "") {
+        alert("Digite um salário!");
+        return;
+    }
+    /*Salva o salário no LocalStorage (armazenamento local do navegador)*/
+    localStorage.setItem("salario", salario);
+    alert("Salário cadastrado com sucesso!");
+    
+})
+/*Carrega o salário que já foi salvo no LocalStorage (se existir) */
+const salarioSalvo = localStorage.getItem("salario");
+
+/*Se houver um salário salvo, preenche o input com ele*/
+if (salarioSalvo) {
+    document.getElementById("salario").value = salarioSalvo;
+}
+
+/*----------ADCIONAR DESPESAS---------- */
+/*Seleciona o formulário de cadastro de despesas e adiciona um evento ao enviar*/
+document.getElementById("formulario").addEventListener("submit",function(cadastrar){
+
+    cadastrar.preventDefault(); /*Evita que a página seja recarregada ao enviar o formulário*/
+
+        var nome = document.getElementById("nome").value
+        var data = document.getElementById("data").value
+        var valor = document.getElementById("valor").value
+
+        /*Objeto: despesa */
+        var despesa = {nome: nome, data: data, valor: parseFloat(valor)}
+
+        /*Recupera a lista de despesas do LocalStorage (ou cria uma lista vazia se não houver)*/
+        var listaDespesas = JSON.parse(localStorage.getItem("lista")) || []
+
+        listaDespesas.push(despesa) /*adiciona nova despesa à lista */
+
+        localStorage.setItem("lista", JSON.stringify (listaDespesas)) /*Salva lista atuaizada no localStorage */
+
+        document.getElementById("formulario").reset() /*limpa formulário */
+        exibirDespesas() /*Chama função para ser exibida na tela */
+})
+
+/*------------EXIBIR DESPESAS----------- */
+function exibirDespesas(){
+
+    /*Recupera a lista de despesas do LocalStorage ou cria lista vazia*/
+    var listaDespesas = JSON.parse(localStorage.getItem("lista")) || []
+    var output = document.getElementById("output") /*seleciona elemento ul */
+    output.innerHTML="" /*limpa conteudo anterior do ul */
+
+    for(let i=0; i<listaDespesas.length; i++){ /*percorre cada despesa da lista */
+        let li = document.createElement("li") /*cria elemento li para cada despesa */
+        li.textContent = "Nome: "+listaDespesas[i].nome + "| Data: "+ listaDespesas[i].data + "| Valor: R$"+listaDespesas[i].valor /*define conteudo da li */
+
+        output.appendChild(li) /*li é exibida dentro da ul */
+    }
+}
+
+/*------------CRIE E EXIBE RESUMO FINANCEIRO------------ */
+/*Seleciona o botão btnexibir e adiciona evento de clique*/
+document.getElementById("btnexibir").addEventListener("click", function(){
+    var listaResumo = document.getElementById("listaResumo")
+    listaResumo.innerHTML="";
+
+    /*Recupera o salário do LocalStorage e converte para número*/
+    var salario = parseFloat(localStorage.getItem("salario"))
+    if(!salario){ /*verifica se há salario cadastrado */
+        alert("Cadastre o salário primeiro!")
+        return;
+    }
+
+    /*Recupera a lista de despesas*/
+    var listaDespesas = JSON.parse(localStorage.getItem("lista"))|| []
+
+    var totalDespesas = 0; /*Inicializa variável para somar todas as despesas*/
+
+    for (let i = 0; i < listaDespesas.length; i++) { /*soma o valor de todas as despesas */
+    totalDespesas += parseFloat(listaDespesas[i].valor); 
+    }
+
+    var saldoFinal = salario - totalDespesas /*calcula saldo final */
+
+
+    /*Cria elementos <li> para mostrar o salário, total de despesas e saldo final*/
+    var liSAL = document.createElement("li")
+    liSAL.style.color="#ffffff"
+    liSAL.textContent = "Seu salário é: R$"+salario
+
+    var liTD = document.createElement("li")
+    liTD.style.color="#ff0000ff"
+    liTD.textContent = "Suas despesas totais são: R$"+ parseFloat(totalDespesas)
+
+    var liSF = document.createElement("li")
+    liSF.style.color = "#3cff00ff"
+    liSF.textContent = "Seu saldo final é: R$"+saldoFinal
+
+    /*adiciona à ul */
+    listaResumo.appendChild(liSAL)
+    listaResumo.appendChild(liTD)
+    listaResumo.appendChild(liSF)
+})
